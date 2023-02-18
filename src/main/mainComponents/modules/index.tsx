867c3f5d4424c44418a1512c4189086d8ce8c372
Sejun3278 <39078283+sejun3278@users.noreply.github.com>
2023-02-18 13:38:17 +0900

    2022-02-18 :: first setting cmm-js

--- src/main/mainComponents/modules/index.tsx
+++ src/main/mainComponents/modules/index.tsx
@@ -1,5 +1,13 @@
-import { Wrapper } from "./styles";
+import { _Modal } from "cmm-js-dev";
 
 export default function ModulesHomePage() {
-  return <Wrapper>이거슨 홈 페이지입니다.</Wrapper>;
+  return (
+    <>
+      <_Modal show={true} onCloseModal={() => {}}>
+        <div>123123123</div>
+      </_Modal>
+      {/* <Modal children={<div>123123</div>}></Modal> */}
+      {/* <div></div> */}
+    </>
+  );
 }
