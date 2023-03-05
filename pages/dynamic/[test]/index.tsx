e24c9f6499d2b8e46cae7cd51d26d83f7f9b8161
Sejun3278 <39078283+sejun3278@users.noreply.github.com>
2023-03-05 20:32:09 +0900

    2022-03-05 :: create docker file

--- pages/dynamic/[test]/index.tsx
+++ pages/dynamic/[test]/index.tsx
@@ -1,12 +1,19 @@
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 
-export default function MCMDynamicPageTestPage() {
+export default function MCMDynamicPageTestPage(props: { test: string }) {
   const router = useRouter();
 
-  return <>{router.query.test} 페이지 입니다.</>;
+  return <>{props.test} 페이지 입니다.</>;
 }
 
-export const getServerSideProps = () => {
+export const getServerSideProps = (context: Router) => {
   // 서버사이드 렌더링 시도시 => out 폴더 생성 안됨 : 빌드 자체가 안됨
   // next.config.js 에서 명령어 추가하기
+  const { test } = context.query;
+
+  return {
+    props: {
+      test,
+    },
+  };
 };
