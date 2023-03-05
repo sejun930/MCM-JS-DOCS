cd4f6aec2ece2495ba061c28ddf844e34e75015f
Sejun3278 <39078283+sejun3278@users.noreply.github.com>
2023-03-05 18:40:57 +0900

    2022-03-05 :: remove export ssr page

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ module.exports = {
   // 마지막에 /를 붙일 것인지? (동적라우팅 시 index.html로 저장할 수 있게 도와준다.)
   generateBuildId: () => "mcm-js-deploy",
   // .next의 빌드 폴더와 out/static 폴더 통일
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/dynamic": { page: "/dynamic" },
+    "/404": { page: "/404" },
+  }),
+  // getServerSideProps가 포함된 페이지 제외하고 빌드
 };
--- pages/dynamic/[test]/index.tsx
+++ pages/dynamic/[test]/index.tsx
@@ -5,3 +5,8 @@ export default function MCMDynamicPageTestPage() {
 
   return <>{router.query.test} 페이지 입니다.</>;
 }
+
+export const getServerSideProps = () => {
+  // 서버사이드 렌더링 시도시 => out 폴더 생성 안됨 : 빌드 자체가 안됨
+  // next.config.js 에서 명령어 추가하기
+};
