ea865d401eabe6a2a19d550071f757d334df1b5f
Sejun3278 <sejun3278@gmail.com>
2023-08-04 01:08:17 +0900

    2023-08-04 :: checking admin login test 1

--- src/main/mainComponents/admin/login/admin.login.container.tsx
+++ src/main/mainComponents/admin/login/admin.login.container.tsx
@@ -97,6 +97,13 @@ export default function AdminLoginPage({
       // 어드민 로그인 정보 가져오기
       const adminInfo = adminLoginInfoData[currentWeek];
 
+      console.log(
+        adminInfo,
+        hashId,
+        hashpw,
+        process.env.NEXT_PUBLIC_ADMIN_ID_D0
+      );
+
       if (hashId !== adminInfo.id || hashpw !== adminInfo.password) {
         // 아이디 & 비밀번호가 불일치 할 경우
         msg = "아이디 & 비밀번호 오류";
