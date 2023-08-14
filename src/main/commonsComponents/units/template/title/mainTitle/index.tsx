52eab1e2c1773420e772c2b0c776ea055993a6ce
Sejun3278 <sejun3278@gmail.com>
2023-08-14 19:49:15 +0900

    2023-08-14 :: fix admin comments render

--- src/main/commonsComponents/units/template/title/mainTitle/index.tsx
+++ src/main/commonsComponents/units/template/title/mainTitle/index.tsx
@@ -5,7 +5,10 @@ import { useRecoilState } from "recoil";
 import { moduleState, versState } from "src/commons/store";
 
 import { moduleRemarksList } from "./data";
-import { moduleIndexList } from "src/commons/data/index/index.commons.data";
+import {
+  initIndexList,
+  moduleIndexList,
+} from "src/commons/data/index/index.commons.data";
 import { _PTextWithHtml, _Title, _Image } from "mcm-js-commons";
 
 import IndexRenderPage from "../../../index/index.render";
@@ -20,9 +23,11 @@ export default function _MainTitleTemplate() {
   // 리스트 가져오기
   useEffect(() => {
     window.setTimeout(() => {
-      if (moduleIndexList(module, vers)) {
-        setList(moduleIndexList(module, vers)());
-      }
+      let moduleList = initIndexList(module);
+      if (moduleIndexList(module, vers))
+        moduleList = moduleIndexList(module, vers)();
+
+      setList(moduleList);
     }, 100);
   }, [module, vers]);
 
--- src/main/mainComponents/modules/tooltip/index.tsx
+++ src/main/mainComponents/modules/tooltip/index.tsx
@@ -6,6 +6,7 @@ import _HowUseForm from "src/main/commonsComponents/units/template/form/howUse";
 import _ExampleForm from "src/main/commonsComponents/units/template/form/example/template.example.container";
 import _PropsForm from "src/main/commonsComponents/units/template/form/props";
 import _TreeForm from "src/main/commonsComponents/units/template/form/tree";
+import _CommentsForm from "src/main/commonsComponents/units/template/form/comments/comments.render";
 
 import { tooltipCodeList } from "./example/tooltip.example.code.data";
 import {
@@ -46,6 +47,7 @@ export default function MyTooltip() {
         />
         <_PropsForm />
         <_TreeForm />
+        <_CommentsForm />
       </ModulesInfoWrapper>
     </Template>
   );
