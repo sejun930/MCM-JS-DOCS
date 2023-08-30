ae814662057e77d3c0b024b1630bb37cf77bc186
Sejun3278 <sejun3278@gmail.com>
2023-08-30 17:38:46 +0900

    2023-08-30 :: test meta tag 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>MCM-JS</title>
         <meta charSet="utf-8" />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website"></meta>
         <meta
           property="og:description"
           content="내 취향대로 모듈을 만들어보세요. || My Custom Modlues"
-        />
+        ></meta>
         <meta name="og:subject" content="" />
         <meta name="og:rating" content="General" />
         <meta
@@ -23,6 +23,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="/images/commons/logo/MCM_white_logo.png"
         />
         <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300px" />
+        <meta property="og:image:height" content="300px" />
       </Head>
       <RecoilRoot>
         <LayoutPage>
