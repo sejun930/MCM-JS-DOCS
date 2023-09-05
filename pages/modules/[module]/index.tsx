965f3c8566db0a9cfb3e327dce727e176b416384
Sejun3278 <sejun3278@gmail.com>
2023-09-05 21:34:32 +0900

    2023-09-05 :: deploy test 9

--- pages/modules/[module]/index.tsx
+++ pages/modules/[module]/index.tsx
@@ -1,30 +1,29 @@
 import MyModal from "src/main/mainComponents/modules/modal";
-// import MyTooltip from "src/main/mainComponents/modules/tooltip";
-// import MySlider from "src/main/mainComponents/modules/slider";
+import MyTooltip from "src/main/mainComponents/modules/tooltip";
+import MySlider from "src/main/mainComponents/modules/slider";
 
-// import ErrorPage from "../../404";
-// import { getCamelCase } from "src/main/commonsComponents/functional";
+import ErrorPage from "../../404";
+import { getCamelCase } from "src/main/commonsComponents/functional";
 
-export default function ModulesPage() {
-  // const Components = ModuleComponentsList[props.name];
-
-  return <MyModal />;
+export default function ModulesPage(props: { name: string }) {
+  const Components = ModuleComponentsList[props.name];
+  return <>{!Components ? <ErrorPage /> : Components}</>;
 }
 
-// export const getServerSideProps = (url: { query: { module: string } }) => {
-//   const { query } = url;
-//   const { module } = query; // 모듈 이름 가져오기
+export const getServerSideProps = (url: { query: { module: string } }) => {
+  const { query } = url;
+  const { module } = query; // 모듈 이름 가져오기
 
-//   return {
-//     props: {
-//       name: getCamelCase(module),
-//     },
-//   };
-// };
+  return {
+    props: {
+      name: getCamelCase(module),
+    },
+  };
+};
 
-// // 모듈 컴포넌트 모음
-// export const ModuleComponentsList: { [key: string]: JSX.Element } = {
-//   Modal: <MyModal />,
-//   Tooltip: <MyTooltip />,
-//   Slider: <MySlider />,
-// };
+// 모듈 컴포넌트 모음
+export const ModuleComponentsList: { [key: string]: JSX.Element } = {
+  Modal: <MyModal />,
+  Tooltip: <MyTooltip />,
+  Slider: <MySlider />,
+};
--- src/main/mainComponents/modules/modal/index.tsx
+++ src/main/mainComponents/modules/modal/index.tsx
@@ -1,71 +1,71 @@
-// import { ModulesInfoWrapper } from "../index.styles";
+import { ModulesInfoWrapper } from "../index.styles";
 
-import { MutableRefObject, useEffect, useRef } from "react";
-// import { useRecoilState } from "recoil";
-// import { versState } from "src/commons/store";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
+import { versState } from "src/commons/store";
 
-// import {
-//   modalExampleList,
-//   modalExampleInitProps,
-//   modalFunctionalData,
-// } from "./example/modal.example.render.data";
-// import { modalCodeList } from "./example/modal.example.code.data";
+import {
+  modalExampleList,
+  modalExampleInitProps,
+  modalFunctionalData,
+} from "./example/modal.example.render.data";
+import { modalCodeList } from "./example/modal.example.code.data";
 
 import Template from "src/main/commonsComponents/units/template/main";
 import _MainTitleTemplate from "src/main/commonsComponents/units/template/title/mainTitle";
-// import _HowUseForm from "src/main/commonsComponents/units/template/form/howUse";
-// import _ExampleForm from "src/main/commonsComponents/units/template/form/example/template.example.container";
-// import _FunctionalForm from "src/main/commonsComponents/units/template/form/functional";
-// import _TreeForm from "src/main/commonsComponents/units/template/form/tree";
-// import _PropsForm from "src/main/commonsComponents/units/template/form/props";
-// import _CommentsForm from "src/main/commonsComponents/units/template/form/comments/comments.render";
-// import CommonsHooksComponents from "src/main/commonsComponents/hooks/commonsHooks";
+import _HowUseForm from "src/main/commonsComponents/units/template/form/howUse";
+import _ExampleForm from "src/main/commonsComponents/units/template/form/example/template.example.container";
+import _FunctionalForm from "src/main/commonsComponents/units/template/form/functional";
+import _TreeForm from "src/main/commonsComponents/units/template/form/tree";
+import _PropsForm from "src/main/commonsComponents/units/template/form/props";
+import _CommentsForm from "src/main/commonsComponents/units/template/form/comments/comments.render";
+import CommonsHooksComponents from "src/main/commonsComponents/hooks/commonsHooks";
 
 export default function MyModal() {
   // 모달을 오픈할 show state (true일 때 모달 오픈)
-  // const { getAllExampleComponentLength } = CommonsHooksComponents();
+  const { getAllExampleComponentLength } = CommonsHooksComponents();
 
-  // const [vers] = useRecoilState(versState);
-  // const [isShow, setIsShow] = useState<Array<boolean>>(
-  //   new Array(getAllExampleComponentLength(modalExampleList(vers))).fill(false)
-  // );
+  const [vers] = useRecoilState(versState);
+  const [isShow, setIsShow] = useState<Array<boolean>>(
+    new Array(getAllExampleComponentLength(modalExampleList(vers))).fill(false)
+  );
 
   // props 페이지의 ref 설정
-  // const propsRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const propsRef = useRef() as MutableRefObject<HTMLDivElement>;
   // fixed 종료 시점 설정
   const endPointRef = useRef() as MutableRefObject<HTMLDivElement>;
   // endPointRef가 있을 경우에만 렌더
-  // const [_, setRender] = useState<boolean>(false);
+  const [render, setRender] = useState<boolean>(false);
 
   useEffect(() => {
     if (endPointRef.current) {
-      // setRender(true);
+      setRender(true);
     }
   }, [endPointRef]);
 
   // 버튼 클릭 시 모달 오픈
-  // const openModal = (idx: number) => (): void => {
-  //   const temp = [...isShow];
-  //   temp[idx] = true;
+  const openModal = (idx: number) => (): void => {
+    const temp = [...isShow];
+    temp[idx] = true;
 
-  //   setIsShow(temp);
-  // };
+    setIsShow(temp);
+  };
 
   // 모달 종료 함수
-  // const closeModal = (idx: number) => (): void => {
-  //   const temp = [...isShow];
-  //   temp[idx] = false;
+  const closeModal = (idx: number) => (): void => {
+    const temp = [...isShow];
+    temp[idx] = false;
 
-  //   setIsShow(temp);
-  // };
+    setIsShow(temp);
+  };
 
   // 예시용 컴포넌트로 전달되는 props
-  // const commonsProps = { isShow, openModal, closeModal };
+  const commonsProps = { isShow, openModal, closeModal };
 
   return (
     <Template>
       <_MainTitleTemplate />
-      {/* <ModulesInfoWrapper ref={endPointRef}>
+      <ModulesInfoWrapper ref={endPointRef}>
         {(render && (
           <>
             <_HowUseForm
@@ -86,7 +86,7 @@ export default function MyModal() {
       </ModulesInfoWrapper>
       <_PropsForm propsRef={propsRef} />
       <_TreeForm />
-      <_CommentsForm /> */}
+      <_CommentsForm />
     </Template>
   );
 }
