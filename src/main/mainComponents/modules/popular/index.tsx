a15b45f6690d180bbaf93d4a2f7a53ccc4bfc04d
Sejun3278 <sejun3278@gmail.com>
2023-10-09 19:54:55 +0900

    2023-10-09 :: init Popular modules with mainTitle

---
+++ src/main/mainComponents/modules/popular/index.tsx
@@ -0,0 +1,38 @@
+import Template from "src/main/commonsComponents/units/template/main";
+import _MainTitleTemplate from "src/main/commonsComponents/units/template/title/mainTitle";
+// import _HowUseForm from "src/main/commonsComponents/units/template/form/howUse";
+// import _ExampleForm from "src/main/commonsComponents/units/template/form/example/template.example.container";
+// import _FunctionalForm from "src/main/commonsComponents/units/template/form/functional";
+// import _PropsForm from "src/main/commonsComponents/units/template/form/props";
+// import _TreeForm from "src/main/commonsComponents/units/template/form/tree";
+// import _CommentsForm from "src/main/commonsComponents/units/template/form/comments/comments.render";
+
+// import { alertCodeList } from "./example/alert.example.code.data";
+// import {
+//   alertExampleList,
+//   initAlertCommonsProps,
+// } from "./example/alert.example.render.data";
+// import { getCommonsHighlight } from "src/commons/highlight";
+// import { alertFunctionalList } from "./functional";
+// import { alertPropsList } from "./props/alert.propsList";
+
+export default function MyPopular() {
+  return (
+    <Template>
+      <_MainTitleTemplate />
+      {/* <_HowUseForm
+        codeInfo={alertCodeList}
+        exmapleContents={getCommonsHighlight.colors("Open Alert").text}
+      />
+      <_ExampleForm
+        exampleList={alertExampleList()}
+        initProps={initAlertCommonsProps}
+        commonsProps={initAlertCommonsProps}
+      />
+      <_FunctionalForm functionalList={alertFunctionalList} />
+      <_PropsForm list={alertPropsList} isObject={true} />
+      <_TreeForm />
+      <_CommentsForm /> */}
+    </Template>
+  );
+}
