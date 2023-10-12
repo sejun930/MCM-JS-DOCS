8fa9b181e056508a4053e34939774e23b10481b7
Sejun3278 <sejun3278@gmail.com>
2023-10-12 11:37:44 +0900

    2023-10-12 :: fixed build

--- src/main/mainComponents/modules/slick/index.tsx
+++ src/main/mainComponents/modules/slick/index.tsx
@@ -1,33 +1,24 @@
-import { Popular } from "mcm-js-dev";
-import { useState } from "react";
+// import { Popular } from "mcm-js-dev";
+// import { useState } from "react";
 
 export default function PopularExamplePage() {
   // 노출될 리스트들을 state 값에 저장합니다. (권장)
-  const [list] = useState([
-    <p> 서해안 골뱅이 500g </p>,
-    <p> 허니레몬 캔디 450p 1.26kg, 1개 </p>,
-    <p> 프로틴 더블 리치 초콜릿 맛, 2.268kg </p>,
-    <p> 국내산 논 우렁살 (냉장), 180g, 1개 </p>,
-    <p> [원두커피1kg] 갓 볶은 신선한 원두커피 1kg </p>,
-  ]);
+  // const [list] = useState([
+  //   <p> 서해안 골뱅이 500g </p>,
+  //   <p> 허니레몬 캔디 450p 1.26kg, 1개 </p>,
+  //   <p> 프로틴 더블 리치 초콜릿 맛, 2.268kg </p>,
+  //   <p> 국내산 논 우렁살 (냉장), 180g, 1개 </p>,
+  //   <p> [원두커피1kg] 갓 볶은 신선한 원두커피 1kg </p>,
+  // ]);
 
-  // 현재 선택되어 있는 리스트의 인덱스 값을 저장합니다.
-  const [select, setSelect] = useState(0);
+  // // 현재 선택되어 있는 리스트의 인덱스 값을 저장합니다.
+  // const [select, setSelect] = useState(0);
 
-  // 현재 선택되어 있는 리스트의 인덱스 값을 가져와 select state 값을 변경하는 함수입니다.
-  const changeSelect = (idx: number) => {
-    // 함수의 props로 해당 리스트의 인덱스 값이 기본값으로 전달됩니다.
-    setSelect(idx);
-  };
+  // // 현재 선택되어 있는 리스트의 인덱스 값을 가져와 select state 값을 변경하는 함수입니다.
+  // const changeSelect = (idx: number) => {
+  //   // 함수의 props로 해당 리스트의 인덱스 값이 기본값으로 전달됩니다.
+  //   setSelect(idx);
+  // };
 
-  return (
-    <div>
-      <p> 현재 선택되어 있는 리스트의 인덱스 번호는? {select} </p>
-      <Popular
-        list={list}
-        minHeight={{ web: 40 }}
-        changeListEvent={changeSelect}
-      />
-    </div>
-  );
+  return <></>;
 }
